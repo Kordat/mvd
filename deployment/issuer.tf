345ba59bb49db220bb6a8ea4136bb74cc04b0988
SergioMedeirosGarcia <sergio.medeiros@galeo.tech>
2026-04-24 11:43:10 +0200

    Remove issuer postgres

--- deployment/issuer.tf
+++ deployment/issuer.tf
@@ -33,45 +33,45 @@ module "dataspace-issuer-vault" {
   namespace         = var.project #kubernetes_namespace.ns.metadata.0.name
 }
 
-# Postgres database for the consumer
-module "dataspace-issuer-postgres" {
-  depends_on       = [kubernetes_config_map_v1.issuer-initdb-config]
-  source           = "./modules/postgres"
-  instance-name    = "issuer"
-  init-sql-configs = ["issuer-initdb-config"]
-  namespace        = var.project #kubernetes_namespace.ns.metadata.0.name
-  image            = "150073872684.dkr.ecr.eu-west-1.amazonaws.com/kordat-dev-postgres:16.3-alpine3.20"
-}
+# # Postgres database for the consumer
+# module "dataspace-issuer-postgres" {
+#   depends_on       = [kubernetes_config_map_v1.issuer-initdb-config]
+#   source           = "./modules/postgres"
+#   instance-name    = "issuer"
+#   init-sql-configs = ["issuer-initdb-config"]
+#   namespace        = var.project #kubernetes_namespace.ns.metadata.0.name
+#   image            = "150073872684.dkr.ecr.eu-west-1.amazonaws.com/kordat-dev-postgres:16.3-alpine3.20"
+# }
 
-# DB initialization for the EDC database
-resource "kubernetes_config_map_v1" "issuer-initdb-config" {
-  metadata {
-    name      = "issuer-initdb-config"
-    namespace = var.project #kubernetes_namespace.ns.metadata.0.name
-  }
-  data = {
-    "issuer-initdb-config.sql" = <<-EOT
-        CREATE USER issuer WITH ENCRYPTED PASSWORD 'issuer' SUPERUSER;
-        CREATE DATABASE issuer;
-        \c issuer issuer
+# # DB initialization for the EDC database
+# resource "kubernetes_config_map_v1" "issuer-initdb-config" {
+#   metadata {
+#     name      = "issuer-initdb-config"
+#     namespace = var.project #kubernetes_namespace.ns.metadata.0.name
+#   }
+#   data = {
+#     "issuer-initdb-config.sql" = <<-EOT
+#         CREATE USER issuer WITH ENCRYPTED PASSWORD 'issuer' SUPERUSER;
+#         CREATE DATABASE issuer;
+#         \c issuer issuer
 
-        create table if not exists membership_attestations
-        (
-            membership_type       integer   default 0,
-            holder_id             varchar                             not null,
-            membership_start_date timestamp default now()             not null,
-            id                    varchar   default gen_random_uuid() not null
-                constraint attestations_pk
-                    primary key
-        );
+#         create table if not exists membership_attestations
+#         (
+#             membership_type       integer   default 0,
+#             holder_id             varchar                             not null,
+#             membership_start_date timestamp default now()             not null,
+#             id                    varchar   default gen_random_uuid() not null
+#                 constraint attestations_pk
+#                     primary key
+#         );
 
-        create unique index if not exists membership_attestation_holder_id_uindex
-          on membership_attestations (holder_id);
+#         create unique index if not exists membership_attestation_holder_id_uindex
+#           on membership_attestations (holder_id);
 
-        -- seed the consumer and provider into the attestations DB, so that they can request FoobarCredentials sourcing
-        -- information from the database
-        INSERT INTO membership_attestations (membership_type, holder_id) VALUES (1, 'did:web:consumer-identityhub%3A7083:consumer');
-        INSERT INTO membership_attestations (membership_type, holder_id) VALUES (2, 'did:web:provider-identityhub%3A7083:provider');
-      EOT
-  }
-}
+#         -- seed the consumer and provider into the attestations DB, so that they can request FoobarCredentials sourcing
+#         -- information from the database
+#         INSERT INTO membership_attestations (membership_type, holder_id) VALUES (1, 'did:web:consumer-identityhub%3A7083:consumer');
+#         INSERT INTO membership_attestations (membership_type, holder_id) VALUES (2, 'did:web:provider-identityhub%3A7083:provider');
+#       EOT
+#   }
+# }
